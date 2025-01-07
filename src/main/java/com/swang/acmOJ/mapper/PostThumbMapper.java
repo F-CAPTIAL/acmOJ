fd7648b7c576eb2cf74ed912d1203eb85ba5ad1c
wangshuai <2310780246@qq.com>
2025-01-07 10:58:21 +0800

    项目初始化

---
+++ src/main/java/com/swang/acmOJ/mapper/PostThumbMapper.java
@@ -0,0 +1,18 @@
+package com.swang.acmOJ.mapper;
+
+import com.swang.acmOJ.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ * @author <a href="https://github.com/liswang">程序员鱼皮</a>
+ * @from <a href="https://swang.icu">编程导航知识星球</a>
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ src/main/java/com/swang/acmOJ/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.swang.acmOJ.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.swang.acmOJ.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * @author <a href="https://github.com/liswang">程序员鱼皮</a>
+ * @from <a href="https://swang.icu">编程导航知识星球</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
